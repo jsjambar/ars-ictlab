c0a06c32e4dc2038c1f777d2993a712efb776e53
Jason Sjambar <realjase@gmail.com>
2018-03-05 09:36:45 +0100

    Initial commit

---
+++ ars-ictlab/ars-ictlab/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ars_ictlab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
