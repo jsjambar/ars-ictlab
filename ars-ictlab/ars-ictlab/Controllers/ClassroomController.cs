576445cdd22750a0268644388621264840e493dd
Jason Sjambar <realjase@gmail.com>
2018-03-05 13:09:36 +0100

    Got a working example for the ajax calls with react. The code can be used, but the controller code needs to be adjusted.

--- ars-ictlab/ars-ictlab/Controllers/ClassroomController.cs
+++ ars-ictlab/ars-ictlab/Controllers/ClassroomController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,6 +15,12 @@ namespace arsictlab.Controllers
         public IActionResult Index()
         {
             return View("Index");
+        }
+
+        public string GetTemperature()
+        {
+            var response = new { error = 0, temperature = new { F = 90, C = 20 } };
+            return JsonConvert.SerializeObject(response);
         }
     }
 }
--- ars-ictlab/ars-ictlab/wwwroot/js/temperature.jsx
+++ ars-ictlab/ars-ictlab/wwwroot/js/temperature.jsx
@@ -1,12 +1,47 @@
-﻿var Temperature = React.createClass({
-  render: function() {
-    return (
-        <div className="temperature">
-        Hello. It's currently 20 degrees in classroom H.x.xxx
-        </div>
-    );
+﻿class Temperature extends React.Component{
+
+  constructor(){
+    super();
+    this.state = {
+      error: 0,
+      temperature: {},
+      isLoaded: false
+    };
   }
-});
+
+  componentDidMount() {
+      fetch("http://localhost:5000/Classroom/GetTemperature")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            error: result.error,
+            temperature: result.temperature
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error: error
+          });
+        }
+      )
+  }
+
+  render() {
+    if(this.state.isLoaded){
+        if (this.state.error == 0) {
+          return <div>Temperatuur in lokaal H4312 is: {this.state.temperature.C} celcius / {this.state.temperature.F} fahrenheit</div>;
+        } else {
+          return <div>this.state.error</div>;
+        }
+    }
+
+    return <div></div>;
+  }
+
+}
 
 ReactDOM.render(
   <Temperature />,
