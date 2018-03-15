085499ca929b47f54e171725ada06e80ef958cb0
Jason Sjambar <realjase@gmail.com>
2018-03-15 11:57:04 +0100

    Selecting a location and classroom shows the classroom's schedule now.

--- ARS/ClientApp/components/classroom/Classrooms.tsx
+++ ARS/ClientApp/components/classroom/Classrooms.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 
-interface ScheduleState { location: String|0, classroom: String|0, selected:Boolean|false }
+interface ScheduleState { location: String|0, classroom: String|0, showSchedule:Boolean|false, iframe:String|"" }
 
 export class Classrooms extends React.Component<RouteComponentProps<{}>, ScheduleState> {
     constructor() {
@@ -9,7 +9,8 @@ export class Classrooms extends React.Component<RouteComponentProps<{}>, Schedul
         this.state = { 
             location: 0,
             classroom: 0,
-            selected: false
+            showSchedule: false,
+            iframe: ""
         };
         this.handleChange = this.handleChange.bind(this);
     }
@@ -26,26 +27,58 @@ export class Classrooms extends React.Component<RouteComponentProps<{}>, Schedul
         });
     }
 
-
     validateValues(){
         const values = this.state;
 
         if(values.location != 0 && values.classroom != 0){
             this.setState({
-                selected: true
+                showSchedule: true,
+                iframe: "https://calendar.google.com/calendar/embed?src=" + this.state.classroom + "@hr.nl&ctz=Europe%2FAmsterdam"
             })
         } else {
             this.setState({
-                selected: true
+                showSchedule: false,
+                iframe: ""
             })
         }
     }
 
     public render() {
         return <div>
-            <h1>Classrooms overview</h1>
-            <p>Please select a location and classroom.</p>
-            
+
+            <div className="page-header">
+                <h1>Classrooms overview</h1>
+                <p>Please select a location and classroom.</p>
+
+                <label>Location</label>
+                <select name='location' value={`${this.state.location}`} onChange={this.handleChange}>
+                    <option value="0">Select a location</option>
+                    <option value="1">Kralingse Zoom</option>
+                    <option value="2">Kralingse Zoom</option>
+                    <option value="3">Kralingse Zoom</option>
+                    <option value="4">Kralingse Zoom</option>
+                </select>
+                
+                <label>Classroom</label>
+                <select name="classroom" value={`${this.state.classroom}`} onChange={this.handleChange}>
+                    <option value="0">Select a classroom</option>
+                    <option value="0907662">H.4.312</option>
+                    <option value="0907662">H.4.312</option>
+                    <option value="0907662">H.4.312</option>
+                    <option value="0907662">H.4.312</option>
+                    H.4.
+                </select>
+                </div>
+
+            {
+                this.state.showSchedule ?
+                <div>
+                    <iframe src={`${this.state.iframe}`} width='100%' height='650' scrolling='no' frameBorder='0'></iframe>
+                </div>
+                :
+                ""
+            }
+
         </div>;
     }
 
