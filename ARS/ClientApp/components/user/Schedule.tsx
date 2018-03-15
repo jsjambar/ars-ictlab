85f7f245f5fa4a9f92d6e1fed05945f84dcf2711
Jason Sjambar <realjase@gmail.com>
2018-03-15 11:24:12 +0100

    Solved the issue for schedule, made a start with the classroom selection.

--- ARS/ClientApp/components/user/Schedule.tsx
+++ ARS/ClientApp/components/user/Schedule.tsx
@@ -22,8 +22,8 @@ export class Schedule extends React.Component<RouteComponentProps<{}>, ScheduleS
         api.getUser(1)
         .then(user => this.setState({
             user: user, 
-            iframe:  "https://calendar.google.com/calendar/embed?src=" + user.Username + "&ctz=Europe%2FAmsterdam"
-        }, () => console.log("user: " + JSON.stringify(user))))
+            iframe:  "https://calendar.google.com/calendar/embed?src=" + user.Username + "@hr.nl&ctz=Europe%2FAmsterdam"
+        }))
         .catch(e => console.log("getUser, " + e))
     }
 
@@ -31,11 +31,12 @@ export class Schedule extends React.Component<RouteComponentProps<{}>, ScheduleS
         return <div>
             <div className="page-header">
                 <h1>Schedule</h1>
+            </div>
+            <div>
                 {
                     this.state.user != 0 && this.state.iframe != "" ?
                     <div>
-                        {console.log(this.state.user.Username)}
-                        <iframe src={`${this.state.iframe}`} width='1200px' height='800' scrolling='no'></iframe>
+                        <iframe src={`${this.state.iframe}`} width='100%' height='650' scrolling='no' frameBorder='0'></iframe>
                     </div>
                     :
                     ""
