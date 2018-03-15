cdfbdb109259c16c3918db6dab98ab6247c6a448
Jason Sjambar <realjase@gmail.com>
2018-03-15 12:34:22 +0100

    shitty form validation for making a reservation is done as well now.

--- ARS/ClientApp/components/classroom/Classrooms.tsx
+++ ARS/ClientApp/components/classroom/Classrooms.tsx
@@ -1,7 +1,16 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import * as api from '../Api';
 
-interface ScheduleState { location: String|0, classroom: String|0, showSchedule:Boolean|false, iframe:String|"" }
+interface ScheduleState { 
+    location: String|0, 
+    classroom: String|0, 
+    description: String|"",
+    start: Number|0,
+    end: Number|0,
+    showSchedule:Boolean|false, 
+    iframe:String|"" 
+}
 
 export class Classrooms extends React.Component<RouteComponentProps<{}>, ScheduleState> {
     constructor() {
@@ -9,10 +18,14 @@ export class Classrooms extends React.Component<RouteComponentProps<{}>, Schedul
         this.state = { 
             location: 0,
             classroom: 0,
+            description: "",
+            start:0,
+            end:0,
             showSchedule: false,
             iframe: ""
         };
         this.handleChange = this.handleChange.bind(this);
+        this.verifyReservation = this.verifyReservation.bind(this);
     }
 
     handleChange(event){
@@ -23,11 +36,11 @@ export class Classrooms extends React.Component<RouteComponentProps<{}>, Schedul
         this.setState({
             [name] : value
         }, () => {
-            const valid = this.validateValues();
+            const valid = this.verifyScheduleSelection();
         });
     }
 
-    validateValues(){
+    verifyScheduleSelection(){
         const values = this.state;
 
         if(values.location != 0 && values.classroom != 0){
@@ -37,10 +50,25 @@ export class Classrooms extends React.Component<RouteComponentProps<{}>, Schedul
             })
         } else {
             this.setState({
-                showSchedule: false,
-                iframe: ""
+                showSchedule: false
             })
         }
+    }
+
+    verifyReservation(){
+        const values = this.state;
+        // refactor this to a re-usable function
+        if(values.location != 0 && values.classroom != 0 && 
+            values.description != "" && values.start != 0 && values.end != 0){
+            this.setReservation();
+            console.log("aishdhdashdsuhdsu");
+        } else {
+            // show errors for the missing values
+        }
+    }
+
+    setReservation(){
+        //api.set_reservation();
     }
 
     public render() {
@@ -49,26 +77,60 @@ export class Classrooms extends React.Component<RouteComponentProps<{}>, Schedul
             <div className="page-header">
                 <h1>Classrooms overview</h1>
                 <p>Please select a location and classroom.</p>
+                <form>
+                    <label>Location</label>
+                    <select name='location' value={`${this.state.location}`} onChange={this.handleChange}>
+                        <option value="0">Select a location</option>
+                        <option value="1">Kralingse Zoom</option>
+                        <option value="2">Kralingse Zoom</option>
+                        <option value="3">Kralingse Zoom</option>
+                        <option value="4">Kralingse Zoom</option>
+                    </select>
+                    <br/>
+                    <label>Classroom</label>
+                    <select name="classroom" value={`${this.state.classroom}`} onChange={this.handleChange}>
+                        <option value="0">Select a classroom</option>
+                        <option value="0907662">H.4.312</option>
+                        <option value="0907662">H.4.312</option>
+                        <option value="0907662">H.4.312</option>
+                        <option value="0907662">H.4.312</option>
+                    </select>
 
-                <label>Location</label>
-                <select name='location' value={`${this.state.location}`} onChange={this.handleChange}>
-                    <option value="0">Select a location</option>
-                    <option value="1">Kralingse Zoom</option>
-                    <option value="2">Kralingse Zoom</option>
-                    <option value="3">Kralingse Zoom</option>
-                    <option value="4">Kralingse Zoom</option>
-                </select>
-                
-                <label>Classroom</label>
-                <select name="classroom" value={`${this.state.classroom}`} onChange={this.handleChange}>
-                    <option value="0">Select a classroom</option>
-                    <option value="0907662">H.4.312</option>
-                    <option value="0907662">H.4.312</option>
-                    <option value="0907662">H.4.312</option>
-                    <option value="0907662">H.4.312</option>
-                    H.4.
-                </select>
-                </div>
+                    <br/>
+
+                    <label>Current room temp: 22</label> {/*todo: this should be updated after classroom+Location has been selected*/}
+                    
+                    <br/>
+
+                    <label>Description:</label>
+                    <textarea name="description" onChange={this.handleChange} value={`${this.state.description}`}></textarea>
+
+                    <br/>
+
+                    <label>Start time:</label>
+                    <select name="start" value={`${this.state.start}`} onChange={this.handleChange}>
+                        <option value="0">Select a start time</option>
+                        <option value="1">9:00</option>
+                        <option value="2">10:00</option>
+                        <option value="3">11:00</option>
+                    </select>
+                    
+                    <br/>
+
+                    <label>End Time:</label>
+                    <select name="end" value={`${this.state.end}`} onChange={this.handleChange}>
+                        <option value="0">Select an end time</option>
+                        <option value="1">9:00</option>
+                        <option value="2">10:00</option>
+                        <option value="3">11:00</option>
+                    </select>
+
+                    <br/>
+
+                    <button type="button" name="make_reservation" onClick={this.verifyReservation}>Make a reservation</button>
+                </form>
+
+            </div>
 
             {
                 this.state.showSchedule ?
