68edb39ef8c1c5ab70242b7e67b0dbea2de437f9
Jason Sjambar <realjase@gmail.com>
2018-03-20 12:29:23 +0100

    re-added startup

---
+++ ARS/Startup.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using React.AspNet;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using JavaScriptEngineSwitcher.Core;
+using JavaScriptEngineSwitcher.ChakraCore;
+using JavaScriptEngineSwitcher.Jint;
+using Microsoft.EntityFrameworkCore;
+using ARS.Models.Contexts;
+using ARS.Models;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+
+namespace ARS
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            // var connection = configuration.GetConnectionString("RConnection");
+            var connection = @"User ID=postgres;Password=geheim;Host=localhost;Port=5005;Database=Reservation_System;Pooling=true;";
+            // services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(connection));
+
+            services.AddDbContext<DatabaseContext>(options =>
+            {
+                options.UseNpgsql(connection);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    HotModuleReplacement = true,
+                    ReactHotModuleReplacement = true
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");    
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
