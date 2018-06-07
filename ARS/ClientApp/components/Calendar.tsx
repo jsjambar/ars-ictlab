c2d360838dd28b09753a1fa8b246fdbe85458300
Jacky Fong <J.wfong@hotmail.com>
2018-06-07 14:19:27 +0200

    error showing

--- ARS/ClientApp/components/Calendar.tsx
+++ ARS/ClientApp/components/Calendar.tsx
@@ -7,11 +7,13 @@ import * as immutable from 'immutable'
 import { ClassroomWithEvents } from '../components/Model'
 import { Link } from 'react-router-dom'
 import "react-big-calendar/lib/css/react-big-calendar.css"
+import { Error } from './Model'
 
 export type CalendarProps = {userid:Number}
 
 interface CalendarState {
-    userReservations: Array<ClassroomWithEvents>
+    userReservations: Array<ClassroomWithEvents>,
+    errors:immutable.List<Error>
 }
 BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment));
 
@@ -19,6 +21,7 @@ export class Calendar extends React.Component<CalendarProps, CalendarState> {
     constructor() {
         super();
         this.state = {
+            errors:immutable.List<Error>(),
             userReservations: Array<ClassroomWithEvents>()
         };
     }
@@ -36,7 +39,14 @@ export class Calendar extends React.Component<CalendarProps, CalendarState> {
     getUserEvents(userid) {
         api.getUserEvents(userid)
             .then(events => this.setUserReservations(events))
-            .catch(e => console.log("getClassroomsWithEvents, " + e))
+            .catch(e => this.set_error({num:15, msg:"User Events Not Found."}))
+    }
+
+    set_error(error:Error){
+        const maybe_error:immutable.List<Error> = this.state.errors.filter(e => e.num == error.num).toList()
+        maybe_error.count() == 0 ?
+            this.setState({...this.state, errors:this.state.errors.push(error)})
+        : null
     }
 
     setUserReservations(events){
@@ -56,6 +66,15 @@ export class Calendar extends React.Component<CalendarProps, CalendarState> {
 
     public render() {
         return <div>
+            <div>
+                {
+                    this.state.errors.map(e => {
+                       return <div className="alert alert-danger" role="alert">
+                            <p>{e.msg}</p>
+                       </div>
+                    })
+                }
+            </div>
             <BigCalendar 
             views={Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])} 
             events={this.state.userReservations} 
