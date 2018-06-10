f515355f10c7e0ba543d1f1976469ab7ac615cba
Mark van der Heiden <Marhek@users.noreply.github.com>
2018-06-10 16:06:22 +0200

    Now you can see the Classroom name for each reservation. Also did cleanup and commenting. Added spacing in classrooms page.

--- ARS/ClientApp/components/Reservation/ReservationEdit.tsx
+++ ARS/ClientApp/components/Reservation/ReservationEdit.tsx
@@ -1,16 +1,24 @@
+// Imports
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import { Location, Classroom, Error } from '../Model';
 import * as api from '../Api';
-import * as immutable from 'immutable'
-import { Location, Classroom, Error } from '../Model'
 import { Link } from 'react-router-dom';
-import DatePicker from 'react-datepicker';
+
+// Helpers
 import * as moment from 'moment';
+import * as immutable from 'immutable';
 import * as helper from '../Datehelper';
+
+// DatePicker
+import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import * as Authentication from '../Authentication'
+
+// Authentication
+import * as Authentication from '../Authentication';
 import { Auth } from '../Authentication';
 
+// State that gets used
 interface ReservationEditState {
     id: 0 | number, 
     date: 0 | Date,
@@ -26,6 +34,7 @@ interface ReservationEditState {
 export class ReservationEdit extends React.Component<RouteComponentProps<{}>, ReservationEditState> {
     constructor(props) {
         super(props);
+        // Default values
         this.state = {
             errors:immutable.List<Error>(),
             id: 0,
@@ -47,28 +56,57 @@ export class ReservationEdit extends React.Component<RouteComponentProps<{}>, Re
         this.getReservation = this.getReservation.bind(this);
     }
 
-    getDate(hour) {
-        const date = new Date();
-        return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), hour);
+    // Begin authentication and getting the startup data
+    componentWillMount() {
+        this.check_auth();
     }
 
+    check_auth() {
+        Authentication.check_auth()
+            .then(r => this.setState({ ...this.state, auth: r }))
+            .then(() => this.handle_auth())
+            .catch(e => this.set_error({ num: 1, msg: "Authentication Failed" }))
+    }
+
+    handle_auth() {
+        this.state.auth.permission == 0 ?
+            window.location.replace('/')
+            : this.state.auth.permission == 2 ?
+                window.location.replace('/admin/classrooms/overview')
+                : this.handle_user()
+    }
+
+    handle_user() {
+        this.setState({ ...this.state, errors: immutable.List<Error>() })
+        const { match: { params } } = this.props;
+        var reservationId = Object.keys(params).map(function (key) { return params[key] })[0];
+        this.getReservation(reservationId);
+    }
+    // End authentication and getting the startup data
+
+    // Handle change of values
     handleChange(event) {
         const target = event.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
 
         this.setState({
-            [name] : value
+            [name]: value
         }, () => this.setStartAndEnd(this.state.timeslot));
     }
 
+    // Unique date onchange method to format the date from the datepicker object
     handleDateChange(date) {
         this.setState({
-          chosen_date: date
-        }) 
+            chosen_date: date
+        })
         this.setDateFromObject(date);
     }
 
+
+    
+
+    // Set the start and end time because we process it in terms of timeslots and save it as start and end time
     setStartAndEnd(chosenTimeslot){
         let processedDate = helper.getDateByTimeslot(chosenTimeslot);
 
@@ -78,6 +116,7 @@ export class ReservationEdit extends React.Component<RouteComponentProps<{}>, Re
         });
     }
 
+    // Gets the date from the datepicker object
     setDateFromObject(obj){
         const self = this;
         Object.keys(obj).map(function(keyName, keyIndex) {
@@ -89,6 +128,7 @@ export class ReservationEdit extends React.Component<RouteComponentProps<{}>, Re
         });
     }
 
+    // Checks if values are valid and then save the reservation
     verifyReservation() {
         const values = this.state;
         // refactor this to a re-usable function
@@ -99,6 +139,7 @@ export class ReservationEdit extends React.Component<RouteComponentProps<{}>, Re
         }
     }
 
+    // Start updating the Reservation
     updateReservation() {
         const values = this.state;
         var res = api.updateReservation(
@@ -127,17 +168,13 @@ export class ReservationEdit extends React.Component<RouteComponentProps<{}>, Re
         }
     }
 
-    set_error(error:Error){
-        const maybe_error:immutable.List<Error> = this.state.errors.filter(e => e.num == error.num).toList()
-        maybe_error.count() == 0 ?
-            this.setState({...this.state, errors:this.state.errors.push(error)})
-        : null
+    // Convert to UTC date
+    getDate(hour) {
+        const date = new Date();
+        return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), hour);
     }
 
-    componentWillMount() {
-        this.check_auth();
-    }
-
+    // Start getting the Reservation that needs to be shown
     getReservation(reservationId) {
         api.getReservation(reservationId)
             .then(reservation => this.setState({
@@ -147,6 +184,7 @@ export class ReservationEdit extends React.Component<RouteComponentProps<{}>, Re
                 end: new Date(reservation.end_time).getHours(),
                 room: reservation.classroom_id,
             }, 
+            // Get Timeslot
             () => {
                 this.setDateFromObject(this.state.chosen_date);
                 let timeslot = helper.getTimeslotByTimes(this.state.start, this.state.end);
@@ -156,26 +194,12 @@ export class ReservationEdit extends React.Component<RouteComponentProps<{}>, Re
             .catch(e => this.set_error({num:5, msg:"No Access"}))
     }
 
-    check_auth(){
-        Authentication.check_auth()
-        .then(r => this.setState({...this.state, auth:r}))
-        .then(() => this.handle_auth())
-        .catch(e => this.set_error({num:1, msg:"Authentication Failed"}))
-    }
- 
-    handle_auth(){
-        this.state.auth.permission == 0 ? 
-            window.location.replace('/')
-        :this.state.auth.permission == 2 ?
-            window.location.replace('/admin/classrooms/overview')
-        : this.handle_user()
-    }
-
-    handle_user(){
-        this.setState({...this.state, errors:immutable.List<Error>()})
-        const { match: { params } } = this.props;
-        var reservationId = Object.keys(params).map(function (key) { return params[key] })[0];
-        this.getReservation(reservationId);
+    // Sets the error to be shown
+    set_error(error: Error) {
+        const maybe_error: immutable.List<Error> = this.state.errors.filter(e => e.num == error.num).toList()
+        maybe_error.count() == 0 ?
+            this.setState({ ...this.state, errors: this.state.errors.push(error) })
+            : null
     }
 
     public render() {
@@ -242,6 +266,7 @@ export class ReservationEdit extends React.Component<RouteComponentProps<{}>, Re
                                 <button type="button" name="create_classroom" className="btn btn-primary" onClick={this.verifyReservation}>Update Reservation</button>
                                 <Link className="btn btn-secondary" to={'/reservation/overview'}>Cancel</Link>
                             </div>
+                            <br />
                         </form>
                     </div>
                 </div>
