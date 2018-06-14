761eb9397d4dfdf96ad7d89142642a0738303541
Mark van der Heiden <Marhek@users.noreply.github.com>
2018-06-14 12:20:59 +0200

    Now the name of the Location will show at the Classroom overview

--- ARS/ClientApp/components/classroom/Classroom.tsx
+++ ARS/ClientApp/components/classroom/Classroom.tsx
@@ -2,15 +2,41 @@ import * as React from 'react';
 import * as immutable from 'immutable'
 import { RouteComponentProps } from 'react-router';
 import { Link } from 'react-router-dom'
-import { Classroom } from '../Model'
+import { Classroom, Location } from '../Model'
 import * as api from '../Api';
 
 export type classroomComponentProps = { classroom: Classroom, key: number }
 
-export class ClassroomComponent extends React.Component<classroomComponentProps, {}> {
+// State that gets used
+interface ClassroomState {
+    location: Location
+}
+
+export class ClassroomComponent extends React.Component<classroomComponentProps, ClassroomState> {
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            location: {
+                id: 0,
+                name: ""
+            }
+        };
+    }
+
+    componentWillMount(): void {
+        this.getLocation();
+    }
+
+    // Start getting the Location where the Classroom is located
+    // This is needed for showing the name of the Location
+    getLocation(): void {
+        api.getLocation(this.props.classroom.location_id)
+            .then(location => { this.setState({ location: location}) })
+            .catch(e => {
+                var tempLocation = this.state.location;
+                tempLocation.name = "Not Found";
+                this.setState({ location: tempLocation })
+            })
     }
 
     confirmDeletion(){
@@ -29,7 +55,7 @@ export class ClassroomComponent extends React.Component<classroomComponentProps,
         return <div className="row">
             <strong className="col-xs-1 first" scope="row">{this.props.classroom.id}</strong>
             <div className="col-xs-2">{this.props.classroom.name}</div>
-            <div className="col-xs-2">{this.props.classroom.location_id}</div>
+            <div className="col-xs-2">{this.state.location.name}</div>
             <div className="col-xs-2">{this.props.classroom.is_public ? "Yes" : "No"}</div>
             <div className="col-xs-2">{this.props.classroom.is_disabled ? "No" : "Yes"}</div>
             <div className="col-xs-3 last">
