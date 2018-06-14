feb509ee03269b9f62d5847509cae808d6a7b940
Andy Bhadai <abhadai@gmail.com>
2018-06-14 14:04:13 +0200

    Added extra unit tests for classrooms

--- ARSTests/ClassroomControllerTests.cs
+++ ARSTests/ClassroomControllerTests.cs
@@ -49,5 +49,50 @@ namespace ARSTests
             var result = this.ClassroomController.Create(null);
             Assert.IsType<BadRequestResult>(result);
         }
+
+        [Fact]
+        public void Classroom_Delete_Also_Deletes_Reservations_And_Tickets()
+        {
+            this.CreateClassroomAndReservationAndTicket();
+
+            // Find first classroom
+            Classroom classroom = this.DatabaseContext.Classrooms.First();
+
+            this.ClassroomController.Delete(classroom.id);
+
+            // Classroom has been deleted
+            Assert.Null(this.DatabaseContext.Classrooms.FirstOrDefault(c => c.id == classroom.id));
+        }
+
+        private void CreateClassroomAndReservationAndTicket()
+        {
+            this.InitializeContext();
+            Random random = new Random();
+            int randomInt = random.Next(40, 100);
+
+            Classroom classroom = new Classroom
+            {
+                id = randomInt
+            };
+
+            Reservation reservation = new Reservation { id = random.Next(100, 200), classroom_id = (int)classroom.id };
+            Ticket ticket = new Ticket { id = random.Next(300, 400), classroom_id = (int)classroom.id };
+
+            this.DatabaseContext.Classrooms.Add(classroom);
+            this.DatabaseContext.Reservations.Add(reservation);
+            this.DatabaseContext.Tickets.Add(ticket);
+        }
+
+        [Fact]
+        public void Classroom_Update_Returns_No_Content()
+        {
+            // Old classroom
+            Classroom classroom = this.DatabaseContext.Classrooms.FirstOrDefault(c => c.id == 1);
+            classroom.name = "WD1001";
+
+            // Classroom.name is not old classroom.name
+            Assert.Equal("WD1001", classroom.name);
+            Assert.IsType<NoContentResult>(this.ClassroomController.Update(1, classroom));
+        }
     }
 }
